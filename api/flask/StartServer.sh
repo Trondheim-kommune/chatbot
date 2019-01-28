fd38e32c36e925c8ebed8751b189ad83d4b788b2
Tinus Flagstad <tinus@flagstad.as>
2019-01-28 12:08:45 +0100

    Add Flask fulfillment response #5

---
+++ api/flask/StartServer.sh
@@ -0,0 +1,2 @@
+export GOOGLE_APPLICATION_CREDENTIALS='/home/agent25/Agent25-google.json'
+FLASK_APP=server.py flask run --host 0.0.0.0 --port=8080
--- api/flask/server.py
+++ api/flask/server.py
@@ -1,12 +1,40 @@
 import json
 from flask import *
+
 app = Flask(__name__)
 
 
+@app.route("/flask", methods=["POST"])
+def get_response():
+    # Read the data sent using POST.
+    json_input_data = json.loads(request.data)
+    try:
+        raw_query_text = json_input_data["queryResult"]["queryText"]
+    except KeyError:
+        raw_query_text = None
 
-@app.route("/", methods= ["POST"])
-def user():
-    data = request.data
-    jsonData = json.loads(data)
-    print(jsonData)
-    return data
+    try:
+        intent = json_input_data["queryResult"]["intent"]["displayName"]
+    except KeyError:
+        intent = None
+
+    try:
+        entities = list(json_input_data["queryResult"]["parameters"].keys())
+    except KeyError:
+        entities = []
+
+    try:
+        default_fulfillment_text = json_input_data["queryResult"]["fulfillmentMessages"][0]["text"]["text"][0]
+    except KeyError:
+        default_fulfillment_text = None
+
+    return json.dumps(
+        {"fulfillmentText": get_fulfillmentText(raw_query_text, intent, entities, default_fulfillment_text)})
+
+
+def get_fulfillmentText(raw_query_text, intent, entities, def_fulfil_text):
+    print(raw_query_text)
+    print(intent)
+    print(entities)
+    print(def_fulfil_text)
+    return "Works"
