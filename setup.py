ecc23d209a505dac7b26dd0422b0ebba3a40d070
Tinus Flagstad <tinus@flagstad.as>
2019-02-08 13:45:24 +0100

    Add controller and add setup.py file and make our project into an actual module #29

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='agent25_project', version='0.1', packages=['api/flask', "model"])
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 virtualenv -p python3 venv
 source venv/bin/activate
+python3 setup.py install
 pip install -r requirements.txt
