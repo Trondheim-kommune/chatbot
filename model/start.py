8d0a5cf168ac8d5887af0077acffe72cdf4a35aa
Tinus Flagstad <tinus@flagstad.as>
2019-02-20 16:50:58 +0100

    When inserting serialized data we will check if something changed since last time and if it did then the ID will be added to conflict_ids #69

--- model/start.py
+++ model/start.py
@@ -20,16 +20,64 @@ def main():
 
     util.set_db(factory)
 
-    # Drop database due to duplicate data when running this
-    factory.get_database().drop_collection("dev")
+    """
+    How we use MongoDB:
+    We have 3 different collections:
+        One for manual entries called "manual"
+        One for production called "prod"
+        One for the in_progress collection called "in_progress"
+    
+    After we have scraped we add all the scraped data into the collection "in_progress" and then 
+    we go through every entry in the "manual" collection and use that entry's ID to query both 
+    prod and in_progress collection. We compare the two contents in prod and in_progress to see
+    if something changed from last time this was run and now. If they do not have the same 
+    content then we need to alert someone that the manual entry needs to be updated.    
+    
+    When this is done in_progress will become our new prod.
+    """
+
+    factory.get_database().drop_collection("in_progress")
 
     print('Starting insertion of {} documents'.format(len(data)))
     pbar = ProgressBar()
     for i, doc in enumerate(pbar(data)):
-        factory.post_document(doc, "dev")
+        factory.post_document(doc, "in_progress")
     print('Successfully inserted {} documents'.format(i + 1))
 
-    util.set_index("dev", factory)
+    manual_documents = factory.get_collection("manual").find()
+
+    # These are the IDs of the documents that are changed in manual and have been changed since
+    # last time.
+    conflict_ids = []
+    for manual_document in manual_documents:
+        if "id" in manual_document:
+            id = manual_document["id"]
+        else:
+            continue
+
+        factory.get_database().get_collection("in_progress").update({"id": id}, {"$set": {
+            "manually_changed": True}})
+
+        prod_match = factory.get_collection("prod").find({"id": id})
+        in_progress_match = factory.get_collection("in_progress").find({"id": id})
+
+        prod_match_doc = next(prod_match, None)
+        in_progress_doc = next(in_progress_match, None)
+
+        if prod_match_doc and in_progress_doc:
+            if prod_match_doc['content'] != in_progress_doc['content']:
+                conflict_ids.append(id)
+
+    print("Conflict IDs are", conflict_ids)
+
+    # Delete the backup prod and rename prod to prod2 and then rename in_progress to prod.
+    factory.get_database().drop_collection("prod2")
+    factory.get_database().get_collection("prod").rename("prod2")
+    factory.get_database().get_collection("in_progress").rename("prod")
+
+    util.set_index("in_progress", factory)
+    util.set_index("prod", factory)
+    util.set_index("manual", factory)
 
 
 if __name__ == '__main__':
