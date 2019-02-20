1154022bbeba2b13d754ecdbbc37290a6d444eef
Anders Hopland <andershopl@hotmail.com>
2019-02-20 22:03:07 +0100

    Adds check for windows or linux in setupfile

--- setup-windows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if ! [ -d "venv" ]; then
-  virtualenv -p python venv
-fi
-source venv/Scripts/activate
-pip install .
-python setup.py install
-pip install -r requirements.txt
--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 if ! [ -d "venv" ]; then
-  virtualenv -p python3 venv
+  if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    virtualenv -p python3 venv
+  elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    virtualenv -p python venv
+  fi
 fi
-source venv/bin/activate
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  source venv/bin/activate
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  source venv/Scripts/activate
+fi
+
 pip install .
 python setup.py install
 pip install -r requirements.txt
