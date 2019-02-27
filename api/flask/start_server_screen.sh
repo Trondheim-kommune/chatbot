a743a2fbd4d6e30c8afde4b3a2c09f3d00daa561
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2019-02-27 15:50:14 +0100

    fixes server startup script paths

--- api/flask/start_server_screen.sh
+++ api/flask/start_server_screen.sh
@@ -1,4 +1,3 @@
 cd /home/agent25/agent-25 && source /home/agent25/agent-25/venv/bin/activate && make
-cd /home/agent25/agent-25/api/flask
 screen -S flask_dev_serv -X quit
 screen -S flask_dev_serv -d -m api/flask/start_server.sh
