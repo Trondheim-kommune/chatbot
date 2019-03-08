9b41b0ba10c022955e27db1a4f6c6981c0c90828
Torjus Iveland <torjusti@gmail.com>
2019-03-08 19:07:55 +0100

    Perform simple query expansion using WordNet

--- model/MongoDBControllerWebhook.py
+++ model/MongoDBControllerWebhook.py
@@ -2,57 +2,77 @@ from model.ModelFactory import ModelFactory
 import model.db_util as util
 from sklearn.metrics.pairwise import cosine_similarity
 from model.keyword_gen import get_tfidf_model
+from model.query_expansion import expand_query
+
+
+NOT_FOUND = 'Jeg fant ikke informasjonen du spurte etter.'
+
+
+def get_corpus_text(doc):
+    ''' Converts a document from the model into a string which will be used in a corpus. '''
+    content = ' '.join(doc['content']['texts'])
+    return doc['content']['title'] + ' ' + content
+
+
+def get_answer_text(doc):
+    ''' Converts a document from the model into a readable string. '''
+    content = ' '.join(doc['content']['texts']) + '\n' + doc['url']
+    return doc['content']['title'] + ':\n' + content
+
+
+def perform_search(query_text):
+    ''' Takes a query string and finds the best matching document in the database. '''
+    # Connect to the database to enable retrieving of documents.
+    factory = ModelFactory.get_instance()
+    util.set_db(factory, db='dev_db')
+
+    # Perform simple query expansion on the original query.
+    query = expand_query(query_text)
+
+    print(query)
+
+    # Retrieve a set of documents using MongoDB. We then attempt to filter these further.
+    docs = factory.get_document(query)
+
+    # Prevent generating an empty corpus if no documents were found.
+    if not docs: return NOT_FOUND
+
+    # Create a corpus on the results from the MongoDB query.
+    corpus = [get_corpus_text(doc) for doc in docs]
+
+    # Create a TF-IDF model on the corpus.
+    vectorizer, corpus_matrix, feature_names = get_tfidf_model(corpus)
+
+    try:
+        # Compare the search query with all documents in our new model using cosine similarity.
+        scores = cosine_similarity(vectorizer.transform([query_text]), corpus_matrix)[0]
+        return get_answer_text(docs[scores.tolist().index(max(scores))])
+    except KeyError:
+        raise Exception('Document does not have content and texts.')
+    except ValueError:
+        return NOT_FOUND
 
 
 class MongoDBControllerWebhook:
-    """
-    This class should be between the dialogflow API and the MongoDB (model).
-    It should handle when a user asks a question.
-    """
-
     def webhook_query(self, raw_query_text, intent, entities, default_text):
-        """
-        Called when a user asks a question.
-        dialogflow --> Flask --> Controller --> model --> controller -->
-        flask --> dialogflow
+        '''
+        Called when a user asks a question in DialogFLow.
 
-        :param raw_query_text: This is the full query text from the user.
-        :param intent: The intent that got automatically matched.
+        :param raw_query_text: The full query text from the user.
+        :param intent: The intent that DialogFlow matched.
         :param entities: The entities that got matched.
-        :param default_text: The randomly chosen static reply if the intent
-        has any.
+        :param default_text: The randomly chosen static reply from the intent, if any.
 
-        :return: This should just return a simple string.
-        """
-        print("raw_query_text:", raw_query_text)
-        print("intent:", intent)
-        print("entities:", entities)
-        print("default_text:", default_text)
+        :return: A string which is the complete answer to the user query.
+        '''
 
-        # Connect to database to retrieve documents
-        factory = ModelFactory.get_instance()
-        util.set_db(factory, db="dev_db")
+        print('raw_query_text:', raw_query_text)
+        print('intent:', intent)
+        print('entities:', entities)
+        print('default_text:', default_text)
 
-        docs = factory.get_document(raw_query_text)
+        result = perform_search(raw_query_text)
 
-        def get_corpus_text(doc):
-            content = ' '.join(doc['content']['texts'])
-            return doc['content']['title'] + ' ' + content
+        print('result:', result)
 
-        def get_answer_text(doc):
-            content = ' '.join(doc['content']['texts']) + '\n' + doc['url']
-            return doc['content']['title'] + ':\n' + content
-
-        corpus = [get_corpus_text(doc) for doc in docs]
-        vectorizer, corpus_matrix, feature_names = get_tfidf_model(corpus)
-
-        try:
-            scores = cosine_similarity(vectorizer.transform([raw_query_text]), corpus_matrix)[0]
-            answer = get_answer_text(docs[scores.tolist().index(max(scores))])
-            print("Answer:", answer)
-            return answer
-        except KeyError:
-            raise Exception("Document doesn't have content and texts. "
-                            "Unable to retrieve text from document in dbcontroller webhook")
-        except ValueError:
-            return "Jeg fant ikke informasjonen du spurte etter."
+        return result
---
+++ model/query_expansion.py
@@ -0,0 +1,57 @@
+import nltk
+from nltk.corpus import wordnet as wn
+import spacy
+from spacy.lemmatizer import Lemmatizer
+from spacy.lang.nb import LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES
+import string
+
+nltk.download('wordnet')
+nltk.download('omw')
+
+# Load a Norwegian language model for Spacy.
+nb = spacy.load('nb_dep_ud_sm')
+
+
+lemmatize = Lemmatizer(LEMMA_INDEX, LEMMA_EXC, LEMMA_RULES)
+
+def expand_query(query):
+    ''' Attempts to expand the given query by using synonyms from WordNet. As
+    a consequnece of this process, the query is also tokenized and lemmatized. '''
+
+    tokens = [
+      # Store tuples of lemmatized tokens and their corresponding POS tags.
+      (lemmatize(token.text, token.pos_)[0], token.pos_) for token in nb(query) 
+      # Filter away punctuation.
+      if not token.text in string.punctuation
+    ]
+
+    result = []
+
+    for token in tokens:
+        # Convert POS tags from Spacy to WordNet.
+        pos = getattr(wn, token[1], None)
+        
+        # Find all synsets for the word, using the Norwegian language.
+        synsets = wn.synsets(token[0], lang='nob')
+
+        if synsets:
+            # Store synonyms in a set, as when there are multiple synsets, we
+            # might end up getting the same lemma names multiple times.
+            synonyms = set()
+
+            for synset in synsets:
+                # Find all lemmas in the synset.
+                for name in synset.lemma_names(lang='nob'):
+                    # Some lemmas contain underscores, which we remove.
+                    synonyms.add(name.replace('_', ' '))
+
+            result += list(synonyms)
+
+            # If we found synonyms, we only add the synonyms. This is because
+            # the original word is already included in the synset, so this
+            # avoids adding it to the result list twice.
+            continue
+        
+        result.append(token[0])
+
+    return ' '.join(result)
