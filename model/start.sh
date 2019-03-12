24ff9ad05bb0d9e202d13d593230899c7f9c7565
Torjus Iveland <torjusti@gmail.com>
2019-03-12 15:59:36 +0100

    Set correct default flags when running scraper programatically

--- model/start.sh
+++ model/start.sh
@@ -2,7 +2,7 @@
 
 if ! [ -f "scraper/scraped.json" ]; then
 	cd scraper
-	scrapy crawl info_gathering -o scraped.json
+	scrapy crawl info_gathering -o scraped.json -a strong_headers=true
 	cd ..
 fi
 python model/start.py scraper/scraped.json
