c60d34aec9dfe1a260581358df69a35db764bf9f
Tinus Flagstad <tinus@flagstad.as>
2019-03-29 13:45:00 +0100

    Change hardcoded home path to ~ #125

--- refresh_data.sh
+++ refresh_data.sh
@@ -1,6 +1,6 @@
 # This file should be called every 24 hours by cron. This deletes the previous scraped data and scrapes $
 # To insert this into cron use the command: `crontab -e` and insert this line:
 # `0 0 * * *  bash /home/agent25/agent-25/refresh_data.sh`
-cd /home/agent25/agent-25
+cd ~/agent-25
 rm scraper/scraped.json
 source venv/bin/activate && bash model/start.sh
