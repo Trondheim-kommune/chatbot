928e87ad28be2a660b74a33976d69e48018356d8
Torjus Iveland <torjusti@gmail.com>
2019-03-31 17:33:33 +0200

    Use all tokens as keywords

--- model/Serializer.py
+++ model/Serializer.py
@@ -1,7 +1,7 @@
 import json
 import copy
 import urllib.request
-from model.keyword_gen import get_keywords, get_tfidf_model
+from model.keyword_gen import tokenize
 from progressbar import ProgressBar
 from util.config_util import Config
 
@@ -30,10 +30,11 @@ class Content:
     def __init__(self, title, texts, keywords=[]):
         self.__title = title
         self.__texts = texts
+
         if keywords:
-            for keyword in keywords:
-                if not isinstance(keyword, KeyWord):
-                    raise TypeError("Must be KeyWord type")
+            if not all(isinstance(keyword, KeyWord) for keyword in keywords):
+                raise TypeError("Must be KeyWord type")
+
         self.__keywords = keywords
 
     def get_content(self):
@@ -71,12 +72,6 @@ class Serializer:
         self.url = url
         self.load_data()
 
-        vectorizer, transformed_corpus, feature_names = self.get_tfidf_model()
-
-        self.__transformed_corpus = transformed_corpus
-        self.__feature_names = feature_names
-        self.__vectorizer = vectorizer
-
     def load_data(self):
         """ Load all JSON data from a file and sets self.__data. Mostly used
         for testing-purposes: real data from scraper is a list of several JSON
@@ -84,14 +79,10 @@ class Serializer:
 
         if self.file_name:
             with open(self.file_name, "r") as f:
-                data = json.load(f)
-                for item in data:
-                    self.__data.append(item)
+                self.__data.extend(json.load(f))
         elif self.url:
             with urllib.request.urlopen(self.url) as url:
-                data = json.loads(url.read().decode())
-                for item in data:
-                    self.__data.append(item)
+                self.__data.extend(json.loads(url.read().decode()))
 
     def get_data(self):
         return self.__data
@@ -99,42 +90,43 @@ class Serializer:
     def get_models(self):
         return self.__models
 
-    def get_tfidf_model(self):
-        corpus = []
-
-        for data in self.__data:
-            queue = list(data['tree'].get('children', []))
-
-            while queue:
-                node = queue.pop(0)
-
-                if 'text' in node:
-                    corpus.append(node['text'])
-
-                if 'children' in node:
-                    queue.append(node['children'])
-
-        return get_tfidf_model(corpus)
-
-    def serialize_data(self):
-        """ Serialize a page object from the web scraper to the data model
-        schema """
-
+    def visit_node(self, data, model_template, models, title):
+        """ Recursively traverse the children and create new Contents from paragraphs. """
         accepted_tags = Config.get_value(["model", "accepted_tags"])
 
+        for child in data:
+            if "children" in child:
+                self.visit_node(child["children"], model_template, models,
+                                title="{} {}".format(title, child["text"]))
+
+            elif child["tag"] in accepted_tags:
+                # Hit a leaf node in recursion tree. We extract the text here and continue.
+                keywords = [KeyWord(token, 1)
+                            for token in tokenize("{} {}".format(title, child["text"]))]
+
+                content = Content(title, [child["text"]], keywords)
+                new_model = copy.deepcopy(model_template)
+                new_model["id"] = child["id"]
+                new_model["content"] = content.get_content()
+                models.append(new_model)
+
+        return models
+
+    def serialize_data(self):
+        """ Serialize a page object from the web scraper to the data model schema. """
         # Iterate over all pages in the JSON data from scraper
-        print('Serializing {} contents'.format(len(self.__data)))
+        print("Serializing {} contents".format(len(self.__data)))
+
         pbar = ProgressBar()
         for data in pbar(self.__data):
-            # TODO: add more metadata
             model = copy.deepcopy(self.__MODEL_SCHEMA)
             model["url"] = data["url"]
 
             # Actual data in the tree
-            if "children" in data["tree"]:
-                child_data = data["tree"]["children"]
-            else:
+            if "children" not in data["tree"]:
                 continue
+
+            child_data = data["tree"]["children"]
 
             # Extract meta keywords if they exist
             if len(child_data) > 0 and child_data[0]["tag"] == "meta":
@@ -145,34 +137,7 @@ class Serializer:
                 # over the rest of the list
                 child_data.pop(0)
 
-            def iterator(idx, data, model_template, models, title):
-                """ Recursively traverse the children and create new Contents
-                from paragraphs """
-                for child in data:
-                    if "children" in child:
-                        # currently just concatenates titles.. need to do
-                        # something more sophisticated here in the future..
-                        # with regards to keyword generation
-                        iterator(idx + 1, child["children"], model_template, models,
-                                 title=title + " " + child["text"])
-                    elif child["tag"] in accepted_tags:
-                        # Hit a leaf node in recursion tree. We extract the
-                        # text here and continue
-                        keywords = [KeyWord(*keyword)
-                                    for keyword in get_keywords(self.__vectorizer,
-                                                                self.__feature_names,
-                                                                title + ' ' + child["text"])]
-
-                        content = Content(title, [child["text"]], keywords)
-                        new_model = copy.deepcopy(model_template)
-                        new_model["id"] = child["id"]
-                        new_model["content"] = content.get_content()
-                        models.append(new_model)
-
-                return models
-
-            models = iterator(0, child_data, model, [], "")
-
+            models = self.visit_node(child_data, model, [], "")
             self.__models += models
 
-        print('Successfully serialized all contents')
+        print("Successfully serialized all contents")
