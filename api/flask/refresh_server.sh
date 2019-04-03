d72a039d9f195373085ccdcceeafe517762d344d
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2019-04-03 15:51:16 +0200

    Limit amount of answers on a param and character count

--- api/flask/refresh_server.sh
+++ api/flask/refresh_server.sh
@@ -1,4 +1,5 @@
 rsync api/flask/ agent25@agent25.tinusf.com:/home/agent25/agent-25/api/flask/ -r
 rsync model/ agent25@agent25.tinusf.com:/home/agent25/agent-25/model/ -r
 rsync scraper/ agent25@agent25.tinusf.com:/home/agent25/agent-25/scraper/ -r
+rsync settings.json agent25@agent25.tinusf.com:/home/agent25/agent-25/settings.json
 ssh agent25@agent25.tinusf.com 'cd /home/agent25/agent-25/api/flask/ && ./start_server_screen.sh'
