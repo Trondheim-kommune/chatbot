4d05623ff7f7469db9441e23afab4a1d650415ea
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2019-08-19 13:24:45 +0200

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='chatbot', version='1.0.3', packages=['chatbot/nlp',
+setup(name='chatbot', version='1.1.0', packages=['chatbot/nlp',
                                                  'chatbot/model',
                                                  'chatbot/api',
                                                  'chatbot/util',
