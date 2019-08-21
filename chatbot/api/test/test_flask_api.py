a72d125979af3921c1a1c516a14315d89aa5390e
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2019-08-21 09:23:17 +0200

    fix: dialogflow updated to allow for duplicate display names

--- chatbot/api/test/test_flask_api.py
+++ chatbot/api/test/test_flask_api.py
@@ -130,35 +130,6 @@ def test_add_and_remove_entities(app):
     assert response_2.status_code == 200
 
 
-def test_add_intents_same_name_throws_exception(app):
-    input_dict = {
-        "type": "batch_create_intents",
-        "data": [
-            {
-                "intent_name": "1",
-                "training_phrases": [
-                    "jeg har en Bmw",
-                    "Jeg har noe som sier vrooooom"
-                ]
-            },
-            {
-                "intent_name": "2",
-                "training_phrases": [
-                    "jeg har også en Bmw",
-                    "Jeg har noe som sier bada bing bada bong"
-                ]
-            }
-        ]
-    }
-    response = app.test_client().put('/v1/dialogflow/intents',
-                                     data=json.dumps(input_dict))
-    assert response.status_code == 400
-    response_json = json.loads(response.data.decode())
-    assert "400 Intent with the display_name '1' already exists." in \
-           response_json["message"]
-    assert "ERROR" in response_json["status"]
-
-
 def test_add_intents(app):
     # Just because I want a unique name I use epoch time here.
     epoch_time = str(int(time.time()))
