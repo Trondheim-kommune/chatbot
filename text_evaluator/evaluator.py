dad85e53a847b8e70ee7883459f8b881a5392d61
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2019-11-17 18:37:54 +0000

    feat #44: generalized and documented path generation

--- text_evaluator/evaluator.py
+++ text_evaluator/evaluator.py
@@ -9,14 +9,11 @@ from chatbot.util.config_util import Config
 class Evaluator():
     _data = []
 
-    _MODEL_SCHEMA = {
-        "id": "",
-        "title": "",
-        "url": "",
-        "content": {}
-    }
-
     _PATH = os.getcwd() + '/evaluation'
+    if not os.path.exists(_PATH):
+        os.mkdir(_PATH)
+    else:
+        raise FileExistsError("The directory evaluation/ already exists!")
 
     def __init__(self, file_name=False):
         self.file_name = file_name
@@ -47,28 +44,50 @@ class Evaluator():
         return get_tfidf_model(corpus)
 
 
+    def _create_path(self, path_length, uri):
+        ''' Build file path to the path_length-th element in uri '''
+        path = self._PATH
+        for i in range(path_length):
+            new_path = path + '/' + uri[i]
+            if not os.path.isdir(new_path):
+                os.mkdir(new_path)
+            path += '/{}'.format(uri[i])
+
+        return path
+
     def serialize_data(self):
         for data in self._data:
-            model = copy.deepcopy(self._MODEL_SCHEMA)
-            model['url'] = data['url']
 
+            # We need to split the URL into it's respective components. 
+            # TODO: The removal of the specific host part should be done throug
+            # the config-file with a regex. It is a bit too specific for trd...
             # * [4:] to remove https://trondheim.kommune.no/x/
             # * strip to remove trailing /
-            uri = model['url'].strip('/').split('/')[4:]
+            uri = data['url'].strip('/').split('/')[4:]
             path = self._PATH
 
             if 'children' not in data['tree']:
-                for i in range(len(uri)):
-                    new_path = path + '/' + uri[i]
-                    if not os.path.isdir(new_path):
-                        os.mkdir(new_path)
-                    path += '/{}'.format(uri[i])
+                # If the uri is not complete (leads to a content-filled page),
+                # we only want to create a dir-path.
+                path_len = len(uri)
+                path = self._create_path(path_len, uri)
+                continue
             else:
-                for i in range(len(uri[:-1])):
-                    new_path = path + '/' + uri[i] + '.html'
-                    if not os.path.exists(new_path):
-                        os.mknod(new_path)
-                    path += '/{}'.format(uri[i])
+                # Whenever uri leads to a leaf-node, we want to end up with an
+                # html-file at the end, not a directory. We therefore exclude
+                # the last part of the uri when building the path, since this
+                # is the name of the html file
+                path_len = len(uri[:-1])
+                path = self._create_path(path_len, uri)
+
+                html_file = path + '/' + uri[-1] + '.html'
+                if not os.path.exists(html_file):
+                    # We don't actually want to make a new file here, instead
+                    # we must copy a pre-defined html-template that we can then
+                     # add DOM-elements to, for each content
+                    os.mknod(html_file)
+    
+                # Serialize and build .html file in var:path
 
 eval_ = Evaluator('chatbot/scraper/scraped.json')
 eval_.serialize_data()
